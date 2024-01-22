dcaab580382d5b633436764cafb720ca59e6c84e
Stéphanie Vanoverberghe <orangestreet@live.fr>
2024-01-22 15:57:50 +0100

    mis en place menu-burger

---
+++ public/assets/js/index.js
@@ -0,0 +1,12 @@
+let iconBurger = document.getElementById("icon-burger");
+console.log(iconBurger);
+
+// function menuBurger() {
+//     if (iconBurger.style.display === "block") {
+//         iconBurger.style.display = "none";
+//     } else {
+//         iconBurger.style.display = "block";
+//     }
+// }
+
+// iconBurger.addEventListener("click", menuBurger)
