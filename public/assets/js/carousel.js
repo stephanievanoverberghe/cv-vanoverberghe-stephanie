d1b73555e8b3a30a0f9beef912c30e1e5c1bdc6a
Stéphanie Vanoverberghe <orangestreet@live.fr>
2024-01-25 16:03:44 +0100

    carousel

---
+++ public/assets/js/carousel.js
@@ -0,0 +1,13 @@
+/* PHASE 1 : Rendre automatique le défilement des slides
+1 - Créer une div avec la class slider-item à l'intérieur de la div slider-items
+2 - Créer une balise img avec les attributs et les class nécessaires
+3 - 
+1 - Passer de slide en slide à l'aide d'une boucle
+*/
+
+
+/* PHASE 2 : Cliquer sur les boutons va a la slide attitrer
+1 - Selectionner un bouton (répéter l'opération sur les autres)
+2 - Cliquer sur un des boutons t'amène à la slide attitré
+3 - Passer la class active sur le bouton sélectionner
+*/
