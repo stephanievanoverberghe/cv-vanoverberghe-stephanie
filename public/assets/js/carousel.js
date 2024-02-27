7ca485b3b850dfcfe49087f3f5d300bd95347ccc
Stéphanie Vanoverberghe <orangestreet@live.fr>
2024-02-27 16:43:10 +0100

    changement color

--- public/assets/js/carousel.js
+++ public/assets/js/carousel.js
@@ -7,7 +7,6 @@
 */
 
 const sliderItems = document.querySelector('.slider-items');
-console.log(sliderItems);
 
 const sliders = [
     {
@@ -15,24 +14,21 @@ const sliders = [
         title: "Vanoverberghe Stéphanie",
         image: 'public/assets/img/slide-01.jpg',
         subtitle: "Développeuse web junior",
-        description: "Passionnée de développement web, je suis à la conquête de mon prochain défi : une alternance dynamique en tant que concepteur développeur d'applications.",
-        visibility: true
+        description: "Passionnée de développement web, je suis à la conquête de mon prochain défi : une alternance dynamique en tant que concepteur développeur d'applications."
     },
     {
         id: 2,
         title: "Compétences techniques",
         image: 'public/assets/img/slide-02.jpg',
         subtitle: "SASS - JS - PHP - SQL",
-        description: "Actuellement concentrée sur l'approfondissement de mes compétences en JavaScript, je complète ma technique avec une exploration continue de SASS, PHP, et SQL.",
-        visibility: false
+        description: "Actuellement concentrée sur l'approfondissement de mes compétences en JavaScript, je complète ma technique avec une exploration continue de SASS, PHP, et SQL."
     },
     {
         id: 3,
         title: "Projets et réalisations",
         image: 'public/assets/img/slide-03.jpg',
         subtitle: "Projets en JS et autres tech",
-        description: "Découvrez mon parcours à travers mes projets clés, qui mettent en lumière mon utilisation pratique de JavaScript, SASS, PHP, et SQL, de la conception à la création.",
-        visibility: false
+        description: "Découvrez mon parcours à travers mes projets clés, qui mettent en lumière mon utilisation pratique de JavaScript, SASS, PHP, et SQL, de la conception à la création."
     },
 ];
 
@@ -43,7 +39,6 @@ const displaySlider = () => {
     const sliderElement = createSliderElement(sliders[currentIndex]);
     sliderElement.classList.add('active');
     sliderItems.appendChild(sliderElement);
-
     updateButton();
 };
 
