bc6a6ea768e272549e5df7ee5b100853c8c6bd5a
Stéphanie Vanoverberghe <orangestreet@live.fr>
2024-02-28 16:51:47 +0100

    chemin

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -12,21 +12,21 @@ const sliders = [
     {
         id: 1,
         title: "Vanoverberghe Stéphanie",
-        image: './src/img/slide-01.jpg',
+        image: 'src/img/slide-01.jpg',
         subtitle: "Développeuse web junior",
         description: "Passionnée de développement web, je suis à la conquête de mon prochain défi : une alternance dynamique en tant que concepteur développeur d'applications."
     },
     {
         id: 2,
         title: "Compétences techniques",
-        image: './src/img/slide-02.jpg',
+        image: 'src/img/slide-02.jpg',
         subtitle: "SASS - JS - PHP - SQL",
         description: "Actuellement concentrée sur l'approfondissement de mes compétences en JavaScript, je complète ma technique avec une exploration continue de SASS, PHP, et SQL."
     },
     {
         id: 3,
         title: "Projets et réalisations",
-        image: './src/img/slide-03.jpg',
+        image: 'src/img/slide-03.jpg',
         subtitle: "Projets en JS et autres tech",
         description: "Découvrez mon parcours à travers mes projets clés, qui mettent en lumière mon utilisation pratique de JavaScript, SASS, PHP, et SQL, de la conception à la création."
     },
